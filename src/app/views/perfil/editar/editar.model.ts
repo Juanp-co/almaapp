8fd8131ef24c05fd302c4839756e316aa072133c
Anthony Velásquez (velantcode) <velasquezthony@gmail.com>
2021-02-12 08:50:19 -0400

    Vista para editar perfil. Ampliada el contenido de la vista del perfil del usuario.

---
+++ src/app/views/perfil/editar/editar.model.ts
@@ -0,0 +1,20 @@
+export interface IEditar {
+  document: string | null;
+  email: string | null;
+  phone: string | null;
+  names: string | null;
+  lastNames: string | null;
+  gender: string | null;
+  birthday: string | null;
+  civilStatus: string | null;
+  educationLevel: string | null;
+  profession: string | null;
+  bloodType: string | null;
+  company: string | null;
+  companyType: string | null;
+  baptized: string | null;
+  department: string | null;
+  city: string | null;
+  locality: string | null;
+  direction: string | null;
+}
