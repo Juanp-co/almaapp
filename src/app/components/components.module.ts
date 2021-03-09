b8c45bcf22c93188406e680d21e7e87742e74a39
Anthony Velásquez (velantcode) <velasquezthony@gmail.com>
2021-03-09 15:44:04 -0400

    Reportes.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {IonicModule} from '@ionic/angular';
 
-import { CoursecardComponent } from './coursecard/coursecard.component';
-import { EventcardComponent } from './eventcard/eventcard.component';
-import { MinheaderComponent } from './minheader/minheader.component';
+import {ChartComponent} from './chart/chart.component';
+import {CoursecardComponent} from './coursecard/coursecard.component';
+import {EventcardComponent} from './eventcard/eventcard.component';
+import {MinheaderComponent} from './minheader/minheader.component';
 import {PaginatorComponent} from './paginator/paginator.component';
-import { PersoncardComponent } from './personcard/personcard.component';
-import { PersoncardtwoComponent } from './personcardtwo/personcardtwo.component';
-import { VideoPlayerComponent } from './video-player/video-player.component';
+import {PersoncardComponent} from './personcard/personcard.component';
+import {PersoncardtwoComponent} from './personcardtwo/personcardtwo.component';
+import {VideoPlayerComponent} from './video-player/video-player.component';
 
 @NgModule({
   declarations: [
+    ChartComponent,
     CoursecardComponent,
     EventcardComponent,
     MinheaderComponent,
@@ -24,6 +26,7 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
     CommonModule, IonicModule
   ],
   exports: [
+    ChartComponent,
     CoursecardComponent,
     EventcardComponent,
     MinheaderComponent,
